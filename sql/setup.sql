396d363d08ff2ac190e14add31462ea93e1130b9
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-10-04 15:13:10 -0700

    model layer

--- sql/setup.sql
+++ sql/setup.sql
@@ -2,10 +2,17 @@ DROP TABLE IF EXISTS animal, species;
 
 CREATE TABLE animal (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name TEXT NOT NULL 
+    animal_name TEXT NOT NULL,
+    extinct BOOLEAN
 );
+INSERT INTO animal (animal_name, extinct)
+    VALUES ('dog', false), ('carolina parakeet', true), ('cat', false) ('bigfoot', true);
 
 CREATE TABLE species (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    name TEXT NOT NULL 
+    animal_name TEXT NOT NULL,
+    species_name TEXT NOT NULL 
 );
+INSERT INTO species (animal_name, species_name)
+    VALUES ('dog', 'mammal'), ('carolina parakeet', 'bird'), ('cat', 'mammal'), ('bigfoot', 'mammal');
+    
