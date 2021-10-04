ffbba678edec864c890db5e285513abcfad2ae78
greg-gilliam <84053399+greg-gilliam@users.noreply.github.com>
2021-10-04 16:10:19 -0700

    more require fixes

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,4 +1,4 @@
-DROP TABLE IF EXISTS animal, species;
+DROP TABLE IF EXISTS animal;
 
 CREATE TABLE animal (
     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,11 +8,10 @@ CREATE TABLE animal (
 INSERT INTO animal (animal_name, extinct)
     VALUES ('dog', false), ('carolina parakeet', true), ('cat', false) ('bigfoot', true);
 
-CREATE TABLE species (
-    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    animal_name TEXT NOT NULL,
-    species_name TEXT NOT NULL 
-);
-INSERT INTO species (animal_name, species_name)
-    VALUES ('dog', 'mammal'), ('carolina parakeet', 'bird'), ('cat', 'mammal'), ('bigfoot', 'mammal');
-    
+-- CREATE TABLE species (
+--     id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+--     animal_name TEXT NOT NULL,
+--     species_name TEXT NOT NULL 
+-- );
+-- INSERT INTO species (animal_name, species_name)
+--     VALUES ('dog', 'mammal'), ('carolina parakeet', 'bird'), ('cat', 'mammal'), ('bigfoot', 'mammal');
